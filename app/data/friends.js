877c45133bc8de934af2b2bff16bde5def0c107c
unknown <lakis004@umn.edu>
2018-08-06 18:00:19 -0500

    initial

---
+++ app/data/friends.js
@@ -0,0 +1,49 @@
+var friendsArray = [
+	{
+		name: 'Michael Scott',
+		photo: 'https://pbs.twimg.com/profile_images/563105554822737921/F-iH3BMT_400x400.png',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Jim Halpert',
+		photo: 'https://pbs.twimg.com/profile_images/3171824697/ef75d90df2e65ce326acf30262df5918_400x400.jpeg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+    },
+    {
+    name:"Ahmed",
+    photo:"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores:[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }]
